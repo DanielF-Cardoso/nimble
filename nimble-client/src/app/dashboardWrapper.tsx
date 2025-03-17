46bbf662d68842634c750d304a9c81bf8276de2b
DanielF-Cardoso <danielfca122@gmail.com>
2025-03-17 16:51:11 -0300

    feat: adicionado estrutura da navbar

---
+++ nimble-client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react'
+import Navbar from './(components)/Navbar'
+
+const DashboardWrapper = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+            <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`} >
+                <Navbar />
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default DashboardWrapper
