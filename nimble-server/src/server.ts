fb8b87e584794e0f3327be16833f9992b366a7fd
DanielF-Cardoso <danielfca122@gmail.com>
2025-03-20 10:21:00 -0300

    feat (backend): criada rota de projetos

--- nimble-server/src/server.ts
+++ nimble-server/src/server.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import projectRoutes from './routes/projectRoutes'
 
 dotenv.config()
 const app = express()
@@ -18,6 +19,8 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+app.use('/api/projects', projectRoutes)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
